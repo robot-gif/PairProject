39708b61c238489fac3de1be1a194e4ade35ef77
robot-gif <57623968+robot-gif@users.noreply.github.com>
2021-03-30 02:19:38 +0800

    导入一些需要用到的图片，编写项目中的一些配置文件

--- 221801204&221801239/frontend/src/App.vue
+++ 221801204&221801239/frontend/src/App.vue
@@ -1,32 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <NavView />
+     <div class="main"><router-view></router-view></div>
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import NavView from "@/components/NavView";
+export default {
+  components: {
+    NavView,
+  },
+};
+</script>
+
+<style lang="less" scoped>
+* {
+  padding: 0;
+  margin: 0;
+  font-family: "微软雅黑",serif;
 }
+.main {
+  height: 100%;
+  width: 100%;
+  background: rgba(53, 64, 68, 1);
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
--- 221801204&221801239/frontend/src/router/index.js
+++ 221801204&221801239/frontend/src/router/index.js
@@ -1,23 +1,46 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import LoginView from '@/components/LoginView'
+import RegisterView from '@/components/RegisterView'
+import SearchView from '@/components/SearchView'
+import ViewArticleView from '@/components/ViewArticleView'
+import chartView from '@/components/chartView'
+import wordCloudView from '@/components/wordCloudView'
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    component: LoginView
+
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/Login',
+    component: LoginView
+  },
+  {
+    path: '/Register',
+    component: RegisterView
+  },
+  {
+    path: '/Search',
+    component: SearchView
+  }, {
+    path: '/ViewArticle',
+    component: ViewArticleView
+  }, {
+    path: '/ChartView',
+    component: chartView
+  },{
+    path: '/WordCloudView',
+    component: wordCloudView
   }
+
 ]
 
 const router = new VueRouter({
